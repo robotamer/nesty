2b0da2ad85192f1273c1e1a305d00b574669c6f7
Ben Corlett <bencorlett@me.com>
2012-04-26 09:38:59 +1000

    Adding root() method to assign new root.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- nesty.php
+++ nesty.php
@@ -1,9 +1,23 @@
 <?php
-
+/**
+ * Part of the Nesty bundle for Laravel.
+ *
+ * @package    Nesty
+ * @version    1.0
+ * @author     Cartalyst LLC
+ * @license    MIT License
+ * @copyright  2012 Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
 namespace Nesty;
 
 use Crud;
 
+/**
+ * Nesty model class.
+ *
+ * @author Ben Corlett
+ */
 class Nesty extends Crud
 {
 	/**
@@ -21,4 +35,24 @@ class Nesty extends Crud
 		'name'  => 'name',
 		'tree'  => 'tree_id',
 	);
+
+	/**
+	 * Makes the current model a root nesty.
+	 *
+	 * @todo Allow existing objects to move to
+	 *       be root objects
+	 *
+	 * @return  Nesty
+	 */
+	public function root()
+	{
+		// Set the left and right limit of the nesty
+		$this->{static::$nesty_cols['left']}  = 1;
+		$this->{static::$nesty_cols['right']} = 2;
+
+		// Tree identifier
+		$this->{static::$nesty_cols['tree']} = (int) $this->query()->max(static::$nesty_cols['tree']) + 1;
+
+		return $this->save();
+	}
 }
