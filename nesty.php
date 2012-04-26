878ec4f0cc8f06555eef8e4f6b2504bcd2dc05cb
Ben Corlett <bencorlett@me.com>
2012-04-26 11:14:37 +1000

    Adding support for new models to become the first child of another model.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- nesty.php
+++ nesty.php
@@ -11,6 +11,7 @@
  */
 namespace Nesty;
 
+use DB;
 use Crud;
 
 /**
@@ -37,12 +38,20 @@ class Nesty extends Crud
 	);
 
 	/**
+	 * An array that contains all children models
+	 * that have been retrieved from the database.
+	 *
+	 * @var array
+	 */
+	public $children = array();
+
+	/**
 	 * Makes the current model a root nesty.
 	 *
 	 * @todo Allow existing objects to move to
-	 *       be root objects
+	 *       be root objects.
 	 *
-	 * @return  Nesty
+	 * @return  bool
 	 */
 	public function root()
 	{
@@ -54,5 +63,127 @@ class Nesty extends Crud
 		$this->{static::$nesty_cols['tree']} = (int) $this->query()->max(static::$nesty_cols['tree']) + 1;
 
 		return $this->save();
+	}
+
+	/**
+	 * Make the current model the first child of
+	 * the given parent.
+	 *
+	 * @param   Nesty  $parent
+	 * @return  Nesty
+	 */
+	public function first_child_of(Nesty &$parent)
+	{
+		return $this->child_of($parent, 'first');
+	}
+
+	/**
+	 * Make the current model the last child of
+	 * the given parent.
+	 *
+	 * @param   Nesty  $parent
+	 * @return  Nesty
+	 */
+	public function last_child_of(Nesty &$parent)
+	{
+		return $this->child_of($parent, 'first');
+	}
+
+	/**
+	 * Make the current model either the first or
+	 * last child of the given parent.
+	 *
+	 * @param   Nesty  $parent
+	 * @return  Nesty
+	 */
+	public function child_of(Nesty &$parent, $position)
+	{
+		if ( ! $parent->exists())
+		{
+			throw new NestyException('The parent Nesty model must exist before you can assign children to it.');
+		}
+
+		if ( ! in_array($position, array('first', 'last')))
+		{
+			throw new NestyException(sprintf('Position %s is not a valid position.', $position));
+		}
+
+		// Reset cached children
+		$parent->children = array();
+
+		// If we haven't been saved to the database before
+		if ( ! $this->exists())
+		{
+			// Inserting as first child
+			if ($position === 'first')
+			{
+				// Our left limit is 1 greater than the left limit
+				// of the parent
+				$this->{static::$nesty_cols['left']} = $parent->{static::$nesty_cols['left']} + 1;
+
+				// Our right is 1 more than our left
+				$this->{static::$nesty_cols['right']} = $parent->{static::$nesty_cols['left']} + 2;
+			}
+
+
+			// Inserting as last child
+			else
+			{
+				// Our left limit is 1 greater than the current last
+				// child's right limit, we will sit as the new last child.
+				// This means also, that our left limit matches the parent's
+				// current right limit. Confusing? Read over it again, it'll make
+				// sense
+				$this->{static::$nesty_cols['left']}  = $parent->{static::$nesty_cols['right']};
+				$this->{static::$nesty_cols['right']} = $parent->{static::$nesty_cols['right']} + 1;
+			}
+
+			// Set our tree identifier to match the parent
+			$this->{static::$nesty_cols['tree']} = $parent->{static::$nesty_cols['tree']};
+
+			// Create a gap a gap in the tree that starts at
+			// our left limit and is 2 wide (the width of an empty
+			// nesty)
+			$this->gap($this->{static::$nesty_cols['left']}, 2, $this->{static::$nesty_cols['tree']});
+
+			$this->save();
+		}
+
+		// If we are existent in the database
+		else
+		{
+
+		}
+	}
+
+	/*
+	|--------------------------------------------------------------------------
+	| Static Usage
+	|--------------------------------------------------------------------------
+	*/
+
+	/**
+	 * Create a gap in the tree.
+	 *
+	 * @param   int  $start
+	 * @param   int  $size
+	 * @param   int  $tree
+	 * @return  void
+	 */
+	protected function gap($start, $size, $tree)
+	{
+		$this->query()
+		      ->where(static::$nesty_cols['left'], '>=', $start)
+		      ->where(static::$nesty_cols['tree'], '=', $tree)
+		      ->update(array(
+		      	static::$nesty_cols['left'] => DB::raw('`'.static::$nesty_cols['left'].'` + '.$size),
+		      ));
+
+		$this->query()
+		      ->where(static::$nesty_cols['right'], '>=', $start)
+		      ->where(static::$nesty_cols['tree'], '=', $tree)
+		      ->update(array(
+		      	static::$nesty_cols['right'] => DB::raw('`'.static::$nesty_cols['right'].'` + '.$size),
+		      ));
 	}
 }
