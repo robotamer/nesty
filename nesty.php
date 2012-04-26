fa76428c7c628c967f65b18cfeeb5b4e4b5ee7ad
Ben Corlett <bencorlett@me.com>
2012-04-26 12:28:21 +1000

    Added full support for making a nesty model the child of any other model.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- nesty.php
+++ nesty.php
@@ -86,7 +86,7 @@ class Nesty extends Crud
 	 */
 	public function last_child_of(Nesty &$parent)
 	{
-		return $this->child_of($parent, 'first');
+		return $this->child_of($parent, 'last');
 	}
 
 	/**
@@ -144,7 +144,7 @@ class Nesty extends Crud
 			// Create a gap a gap in the tree that starts at
 			// our left limit and is 2 wide (the width of an empty
 			// nesty)
-			$this->gap($this->{static::$nesty_cols['left']}, 2, $this->{static::$nesty_cols['tree']});
+			$this->gap($this->{static::$nesty_cols['left']});
 
 			$this->save();
 		}
@@ -152,8 +152,101 @@ class Nesty extends Crud
 		// If we are existent in the database
 		else
 		{
+			// Remove from tree and reload
+			$this->remove_from_tree();
 
+			// Reload parent
+			$parent->reload();
+
+			// Determine our new left position
+			$new_left = ($position === 'first') ? $parent->{static::$nesty_cols['left']} + 1 : $parent->{static::$nesty_cols['right']};
+
+			// Reinsert in tree
+			$this->reinsert_in_tree($new_left);
 		}
+	}
+
+	/**
+	 * Get the size in the tree of this nesty.
+	 *
+	 * @return  int
+	 */
+	public function size()
+	{
+		return $this->{static::$nesty_cols['right']} - $this->{static::$nesty_cols['left']};
+	}
+
+	/**
+	 * Reloads the current model from the database.
+	 *
+	 * @return  Nesty
+	 */
+	public function reload()
+	{
+		if ( ! $this->exists())
+		{
+			throw new NestyException('You cannot call reload() on a model that hasn\'t been persisted to the database');
+		}
+
+		$this->fill($this->query()->where(static::$key, '=', $this->{static::$key})->first());
+
+		return $this;
+	}
+
+	/**
+	 * Used to keep a nesty model in the database but
+	 * remove it from the tree structure. Used when
+	 * moving nesties around.
+	 *
+	 * @return  Nesty
+	 */
+	protected function remove_from_tree()
+	{
+		// We need to move our nesty so it's outside the tree.
+		// For this, our change must bring our right limit to be
+		// 0.
+		$delta = 0 - $this->{static::$nesty_cols['right']};
+
+		// Move this model and it's children outside
+		// the tree by changing all limits by the delta
+		// calculdated above
+		$this->query()
+		     ->where(static::$nesty_cols['left'], 'BETWEEN', DB::raw($this->{static::$nesty_cols['left']}.' AND '.$this->{static::$nesty_cols['right']}))
+		     ->where(static::$nesty_cols['tree'], '=', $this->{static::$nesty_cols['tree']})
+		     ->update(array(
+		     	static::$nesty_cols['left'] => DB::raw('`'.static::$nesty_cols['left'].'` + '.$delta),
+		     	static::$nesty_cols['right'] => DB::raw('`'.static::$nesty_cols['right'].'` + '.$delta),
+		     ));
+
+		// Remove the gap we created - notice the '-' on the second param
+		$this->gap($this->{static::$nesty_cols['left']}, - ($this->size() + 1));
+
+		return $this->reload();
+	}
+
+	/**
+	 * Reverse of Nesty::remove_from_tree(). Used to
+	 * reinsert a nesty back into the tree structure
+	 *
+	 * @param   int  $left
+	 * @return  Nesty
+	 */
+	protected function reinsert_in_tree($left)
+	{
+		// Create a gap
+		$this->gap($left);
+
+		// Reinsert in new gap by moving everything between
+		// the limits the right delta 
+		$this->query()
+		     ->where(static::$nesty_cols['left'], 'BETWEEN', DB::raw((0 - $this->size()).' AND 0'))
+		     ->where(static::$nesty_cols['tree'], '=', $this->{static::$nesty_cols['tree']})
+		     ->update(array(
+		     	static::$nesty_cols['left'] => DB::raw('`'.static::$nesty_cols['left'].'` + '.($left + $this->size())),
+		     	static::$nesty_cols['right'] => DB::raw('`'.static::$nesty_cols['right'].'` + '.($left + $this->size())),
+		     ));
+
+		return $this;
 	}
 
 	/*
@@ -170,8 +263,18 @@ class Nesty extends Crud
 	 * @param   int  $tree
 	 * @return  void
 	 */
-	protected function gap($start, $size, $tree)
+	protected function gap($start, $size = null, $tree = null)
 	{
+		if ($size === null)
+		{
+			$size = $this->size() + 1;
+		}
+
+		if ($tree === null)
+		{
+			$tree = $this->{static::$nesty_cols['tree']};
+		}
+
 		$this->query()
 		      ->where(static::$nesty_cols['left'], '>=', $start)
 		      ->where(static::$nesty_cols['tree'], '=', $tree)
